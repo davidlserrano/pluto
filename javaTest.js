55c928c1c6db75aa999d1adca14bdfb3b581713a
davidlserrano <47878688+davidlserrano@users.noreply.github.com>
2019-03-10 20:08:32 -0700

    Add files via upload

---
+++ javaTest.js
@@ -0,0 +1,25 @@
+var counter = 0;
+
+
+var timer = $('#counter');
+
+function timeIt () {
+		counter++;
+		timer.html(counter);
+	}
+setInterval(timeIt, 1000);
+
+
+
+
+function repeat(){
+	$('.rocket').css('margin-left','14%');
+	$('.rocket').animate({
+		left: "59%"
+	}, 1000, function(){
+		$('.rocket').repeat();
+	}
+	);	
+}
+						 
+setInterval(repeat, 1000);
